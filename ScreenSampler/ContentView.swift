e70758aeaf83db5f311353a66bf1630a311b8302
elaniacs <carensousaesilva@gmail.com>
2024-04-21 10:53:52 -0300

    Initial Commit

---
+++ ScreenSampler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenSampler
+//
+//  Created by Cáren on 21/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenSampler/ScreenSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenSamplerApp.swift
+//  ScreenSampler
+//
+//  Created by Cáren on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
